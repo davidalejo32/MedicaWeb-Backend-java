820f599a7ba265b67856b22cad1c81faa5c41dec
David Claros <davidalejo32@hotmail.com>
2023-07-18 20:59:27 -0500

    V 0.0.1

---
+++ src/main/java/com/medicaweb/medicaweb/MedicawebApplication.java
@@ -0,0 +1,13 @@
+package com.medicaweb.medicaweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicawebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicawebApplication.class, args);
+	}
+
+}
